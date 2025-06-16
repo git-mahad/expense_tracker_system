8d5ecdeec50f1f7efb3b6f8f1f3e3f3752f410d2
Muhammad Mahad <mahad.dev3@gmail.com>
2025-06-16 17:56:54 +0500

    initial commit

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,20 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength, IsOptional, IsEnum, isNotEmpty } from 'class-validator';
+import { UserRole } from '../entities/user.entity';
+
+export class RegisterDto{
+
+  @IsNotEmpty()
+  name: string
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string
+  
+  @IsString()
+  @MinLength(3, {message: 'length must be > 3'})
+  password: string
+
+  @IsOptional()
+  @IsEnum(UserRole)
+  role?:UserRole
+}
---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,30 @@
+import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn, OneToMany } from 'typeorm';
+import { Exclude } from 'class-transformer';
+
+export enum UserRole {
+  ADMIN = 'admin',
+  USER = 'user',
+}
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  @Exclude()
+  password: string;
+
+  @Column({
+    type: 'enum',
+    enum: UserRole,
+    default: UserRole.USER
+  })
+  role: UserRole
+}
