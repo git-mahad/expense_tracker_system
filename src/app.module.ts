8df036bca8eb76c0da1a5ddbaff7bf2cead37325
Muhammad Mahad <mahad.dev3@gmail.com>
2025-06-19 20:43:33 +0500

    fixed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
-// import { User } from './auth/entities/user.entity';
+import { User } from './auth/entities/user.entity';
 
 @Module({
   imports: [
@@ -15,19 +15,18 @@ import { AuthModule } from './auth/auth.module';
       useFactory: (config: ConfigService) => ({
         type: 'mysql',
         host: config.get('DB_HOST'),
-        port: +config.get<number>('DB_PORT')!, 
+        port: config.get<number>('DB_PORT'),
         username: config.get('DB_USERNAME'),
         password: config.get('DB_PASSWORD'),
         database: config.get('DB_NAME'),
-        entities: [],
-        autoLoadEntities: true,
+        entities: [User],
         synchronize: true,
       }),
     }),
-    
+    TypeOrmModule.forFeature([User]),
     AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
